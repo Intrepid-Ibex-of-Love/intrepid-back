27510b69322ef5cabc00a022129a1095c2ece8d8
Cristina Bosa <cristinabosasanchez@gmail.com>
2021-05-29 20:01:37 +0100

    creacion del .env y ormconfig.js

--- src/server.ts
+++ src/server.ts
@@ -14,18 +14,7 @@ import * as cors from 'cors';
 import { ProductMedia } from "./entity/ProdutcMedia";
 
 
-createConnection({
-    "type": "mysql",
-    "host": "localhost",
-    "port": 3306,
-    "username": "root",
-    "password": "",
-    "database": "community",
-    "synchronize": true,
-    "logging": false,
-    "entities": [User, Category, Product, ProductMedia],
-}
-).then(async connection => {
+createConnection().then(async connection => {
 
     const port = 3002;
     // create express app
