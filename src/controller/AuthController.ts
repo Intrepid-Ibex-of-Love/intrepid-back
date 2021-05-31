536e2ae26b460382cfce78f57bf7309fdea46b28
Alejandro <alejandroasc96@gmail.com>
2021-05-31 19:25:58 +0100

    comienzo

--- src/controller/AuthController.ts
+++ src/controller/AuthController.ts
@@ -29,7 +29,7 @@ export class AuthController {
                         user: userFound,
                         token: token
                     })
-                }
+                } 
 
 
                     
