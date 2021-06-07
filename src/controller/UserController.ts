341f4c9bec38498b643fe303269f942f54b0ddc5
Melo 'adabaed' Ortega <carmeloortegavalido@outlook.com>
2021-06-07 02:51:07 +0100

    cambios

--- src/controller/UserController.ts
+++ src/controller/UserController.ts
@@ -1,4 +1,4 @@
-import { getRepository } from "typeorm";
+import { createQueryBuilder, getRepository } from "typeorm";
 import { NextFunction, Request, Response } from "express";
 import { User } from "../entity/User";
 
