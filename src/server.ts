e7c59eff6f58b496a2a2c8c1fdc5e55c95599403
Cristina Bosa <cristinabosasanchez@gmail.com>
2021-06-17 10:33:47 +0100

    - Eliminar tabla de categorías y el controller

--- src/server.ts
+++ src/server.ts
@@ -34,17 +34,6 @@ createConnection()
                 }
             });
         });
-        CategoryRouter.forEach(route => {
-            (app as any)[route.method](route.route, (req: Request, res: Response, next: Function) => {
-                const result = (new (route.controller as any))[route.action](req, res, next);
-                if (result instanceof Promise) {
-                    result.then(result => result !== null && result !== undefined ? res.send(result) : undefined);
-
-                } else if (result !== null && result !== undefined) {
-                    res.json(result);
-                }
-            });
-        });
         ProductRouter.forEach(route => {
             (app as any)[route.method](route.route, (req: Request, res: Response, next: Function) => {
                 const result = (new (route.controller as any))[route.action](req, res, next);
